3c1fbef1160121e6bc6a9db05caefb04219da29d
abirmajumdar <a13828351@gmail.com>
2025-05-04 02:56:46 +0530

    rr

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 from flask_cors import CORS
 import joblib
 import re
@@ -328,4 +329,6 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))  # Use the PORT env variable or default to 5000
+   app.run(host="0.0.0.0", port=port, debug=True)
+
